cff70a4c8c3c2307d3ff4314c457067501c86483
toxidity-18 <desavigatunganga@gmail.com>
2024-09-01 11:28:24 +0300

    first

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
-
 class Dog:
-    # Class body goes here
+    def __init__(self, name):
+        self.name = name
 
-    #Instance method definition
-    pass
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.")
+
+    def roll_over(self):
+        print("The dog is rolling over.")
